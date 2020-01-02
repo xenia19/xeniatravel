41859e96903d8adf9fb8b8e096434467d69d57ad
Xenia <XENIA.V.GALAKTIONOVA@YANDEX.RU>
2020-01-02 17:16:32 +0500

    Updates

---
+++ precache-manifest.51ef55450679ebfd999f026e9be905f2.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e784b752b86caa3d87297c533191f90c",
+    "url": "/xeniatravel/index.html"
+  },
+  {
+    "revision": "2c8e35726f597e4f1f57",
+    "url": "/xeniatravel/static/css/2.34c9b3d3.chunk.css"
+  },
+  {
+    "revision": "6209093ea20d1c5f0980",
+    "url": "/xeniatravel/static/css/main.233f9ece.chunk.css"
+  },
+  {
+    "revision": "2c8e35726f597e4f1f57",
+    "url": "/xeniatravel/static/js/2.f5a41459.chunk.js"
+  },
+  {
+    "revision": "8dbc28b25aa4971eb27411d63f6aa7d3",
+    "url": "/xeniatravel/static/js/2.f5a41459.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6209093ea20d1c5f0980",
+    "url": "/xeniatravel/static/js/main.49c333a3.chunk.js"
+  },
+  {
+    "revision": "4504c2e532cf4274c685",
+    "url": "/xeniatravel/static/js/runtime-main.d11d7e80.js"
+  },
+  {
+    "revision": "56533e8a636a7385e8777b2319da2c8b",
+    "url": "/xeniatravel/static/media/43.56533e8a.jpg"
+  },
+  {
+    "revision": "63eab5afbe7ba161df8b0ed7390fa306",
+    "url": "/xeniatravel/static/media/alpaca.63eab5af.jpg"
+  },
+  {
+    "revision": "97c13d24a8323de56198e916f87eb4c5",
+    "url": "/xeniatravel/static/media/argentina_04.97c13d24.jpg"
+  },
+  {
+    "revision": "b730f207a38e0456b0cce39f77d95e13",
+    "url": "/xeniatravel/static/media/argentina_map.b730f207.png"
+  },
+  {
+    "revision": "1863a15065a6210a838e90f451affa66",
+    "url": "/xeniatravel/static/media/avocado.1863a150.png"
+  },
+  {
+    "revision": "7a52fd86c47ff95dddb366337c4f87f3",
+    "url": "/xeniatravel/static/media/buenos_aires01.7a52fd86.jpg"
+  },
+  {
+    "revision": "a46c2537fc7e9331ff9296ab8d0aa3d9",
+    "url": "/xeniatravel/static/media/dog1.a46c2537.JPG"
+  },
+  {
+    "revision": "6be301519858a5e37eb3a42c44e145c7",
+    "url": "/xeniatravel/static/media/dog2.6be30151.JPG"
+  },
+  {
+    "revision": "d64215a2b080fb307752857c6b38f898",
+    "url": "/xeniatravel/static/media/dog3.d64215a2.JPG"
+  },
+  {
+    "revision": "9f567e0f7950fd5eb5c0eb6f9772588c",
+    "url": "/xeniatravel/static/media/dogs4.9f567e0f.jpg"
+  },
+  {
+    "revision": "1832f142ce27f9795b48b08ae680d109",
+    "url": "/xeniatravel/static/media/dogs5.1832f142.jpg"
+  },
+  {
+    "revision": "19a759f16af9e9bfcd387e69878697d9",
+    "url": "/xeniatravel/static/media/dogs6.19a759f1.jpg"
+  },
+  {
+    "revision": "48cbc03f75a7bb4daa5295d72d94214f",
+    "url": "/xeniatravel/static/media/dogs7.48cbc03f.jpg"
+  },
+  {
+    "revision": "730f4b97dddfabdb359563c15d907256",
+    "url": "/xeniatravel/static/media/dogs8.730f4b97.jpg"
+  },
+  {
+    "revision": "ac271fc7bb9cd4de7fe166de3abc96c8",
+    "url": "/xeniatravel/static/media/dogs9.ac271fc7.jpg"
+  },
+  {
+    "revision": "200086fecd991d65a28ce1b32c115280",
+    "url": "/xeniatravel/static/media/jacaranda.200086fe.jpg"
+  },
+  {
+    "revision": "31a6d561c5f6b15597c7f4384840c81f",
+    "url": "/xeniatravel/static/media/laplata.31a6d561.jpg"
+  },
+  {
+    "revision": "c38c1128630450e17d1a698d0a5594bc",
+    "url": "/xeniatravel/static/media/lecce.c38c1128.jpg"
+  },
+  {
+    "revision": "7739e4a64d03a7f5732660ade1c13d01",
+    "url": "/xeniatravel/static/media/machu.7739e4a6.jpg"
+  },
+  {
+    "revision": "2260bdac96f5feaa84bd983dd623a132",
+    "url": "/xeniatravel/static/media/map.2260bdac.png"
+  },
+  {
+    "revision": "9d61b29c00465fe8e21ae4a10e913043",
+    "url": "/xeniatravel/static/media/map_footer.9d61b29c.png"
+  },
+  {
+    "revision": "ace9798a3c150c5072743e2acf7e6d82",
+    "url": "/xeniatravel/static/media/mar.ace9798a.jpg"
+  },
+  {
+    "revision": "5f4bf31f17208b2babda2f5d9470deeb",
+    "url": "/xeniatravel/static/media/mate.5f4bf31f.jpg"
+  },
+  {
+    "revision": "3492b9111c5001c26754afba61b09e70",
+    "url": "/xeniatravel/static/media/mate_1.3492b911.jpg"
+  },
+  {
+    "revision": "14249dd02685db354747239f48d991db",
+    "url": "/xeniatravel/static/media/me.14249dd0.jpeg"
+  },
+  {
+    "revision": "5b77972c47d132adc78de8030a59233f",
+    "url": "/xeniatravel/static/media/mexico_map.5b77972c.jpg"
+  },
+  {
+    "revision": "51cc26798d29b274fdd33014b752b819",
+    "url": "/xeniatravel/static/media/palms.51cc2679.png"
+  },
+  {
+    "revision": "4633ac17ea28f58f7a08bdc12771936e",
+    "url": "/xeniatravel/static/media/plane_button.4633ac17.png"
+  },
+  {
+    "revision": "b42c255be7a2b9616257027f58d03a09",
+    "url": "/xeniatravel/static/media/quesadillas.b42c255b.jpg"
+  },
+  {
+    "revision": "6f5bedbcf0e989daad2325091adeb3b5",
+    "url": "/xeniatravel/static/media/rio.6f5bedbc.jpg"
+  },
+  {
+    "revision": "d089550010ca2bf1fd3802ae3b3075c9",
+    "url": "/xeniatravel/static/media/spain.d0895500.png"
+  },
+  {
+    "revision": "ef74a9f7f064d587bd57ccf2c1d377c9",
+    "url": "/xeniatravel/static/media/whole_map.ef74a9f7.png"
+  }
+]);
